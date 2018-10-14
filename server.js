53e45ab52f0197c73289a55013749f6a92506fd7
Ernest Oliver Casanes <ecasanes@gmail.com>
2018-10-14 17:31:03 +0800

    init node server

---
+++ server.js
@@ -0,0 +1,10 @@
+let express = require('express');
+let app = express();
+
+/* app.get('/', (req, res) => {
+  res.send('Hello World');
+}); */
+
+app.use(express.static(__dirname + '/public'))
+
+app.listen(3000);
